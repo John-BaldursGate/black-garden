aca2bc302de7268ed58c067aca0334ddba85edcc
John-BaldursGate <163097006+John-BaldursGate@users.noreply.github.com>
2025-09-28 07:34:35 +0000

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,28 +10,28 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # remove unwanted packages
-dnf remove firefox firefox-langpacks gnome-software gnome-software-ostree gnome-tour yelp malcontent-control htop nvtop -y
+dnf5 remove firefox firefox-langpacks gnome-software gnome-software-ostree gnome-tour yelp malcontent-control htop nvtop -y
 
 # this installs a package from fedora repos
-dnf install btop distrobox fastfetch libguestfs-tools libvirt-daemon-config-network libvirt-daemon-kvm mangohud python3-libguestfs qemu-kvm rocm-smi steam-devices virt-install virt-manager virt-top virt-viewer wine -y
+dnf5 install btop distrobox fastfetch libguestfs-tools libvirt-daemon-config-network libvirt-daemon-kvm mangohud python3-libguestfs qemu-kvm rocm-smi steam-devices virt-install virt-manager virt-top virt-viewer wine -y
 
 # rpm fusion
-dnf -y install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-dnf -y install steam
-dnf -y remove rpmfusion-free-release rpmfusion-nonfree-release
+dnf5 -y install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+dnf5 -y install steam
+dnf5 -y remove rpmfusion-free-release rpmfusion-nonfree-release
 
-dnf -y copr enable atim/heroic-games-launcher
-dnf -y copr enable ilyaz/LACT
-dnf -y install heroic-games-launcher-bin
-dnf -y install lact
-dnf -y copr disable atim/heroic-games-launcher
-dnf -y copr disable ilyaz/LACT
+dnf5 -y copr enable atim/heroic-games-launcher
+dnf5 -y copr enable ilyaz/LACT
+dnf5 -y install heroic-games-launcher-bin
+dnf5 -y install lact
+dnf5 -y copr disable atim/heroic-games-launcher
+dnf5 -y copr disable ilyaz/LACT
 # Use a COPR Example:
 #
-# dnf -y copr enable ublue-os/staging
-# dnf -y install package
+# dnf5 -y copr enable ublue-os/staging
+# dnf5 -y install package
 # Disable COPRs so they don't end up enabled on the final image:
-# dnf -y copr disable ublue-os/staging
+# dnf5 -y copr disable ublue-os/staging
 
 #### Example for enabling a System Unit File
 

94937b4466e6f5e01adc5e0892525e94b6ea8a65
John-BaldursGate <163097006+John-BaldursGate@users.noreply.github.com>
2025-09-28 07:22:32 +0000

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,23 +10,28 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # remove unwanted packages
-dnf5 remove firefox firefox-langpacks gnome-software gnome-software-ostree gnome-tour yelp malcontent-control htop nvtop -y
+dnf remove firefox firefox-langpacks gnome-software gnome-software-ostree gnome-tour yelp malcontent-control htop nvtop -y
 
 # this installs a package from fedora repos
-dnf5 install btop distrobox fastfetch libguestfs-tools libvirt-daemon-config-network libvirt-daemon-kvm mangohud python3-libguestfs qemu-kvm rocm-smi steam steam-devices virt-install virt-manager virt-top virt-viewer wine -y
+dnf install btop distrobox fastfetch libguestfs-tools libvirt-daemon-config-network libvirt-daemon-kvm mangohud python3-libguestfs qemu-kvm rocm-smi steam-devices virt-install virt-manager virt-top virt-viewer wine -y
 
-dnf5 -y copr enable atim/heroic-games-launcher
-dnf5 -y copr enable ilyaz/LACT
-dnf5 -y install heroic-games-launcher-bin
-dnf5 -y install lact
-dnf5 -y copr disable atim/heroic-games-launcher
-dnf5 -y copr disable ilyaz/LACT
+# rpm fusion
+dnf -y install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+dnf -y install steam
+dnf -y remove rpmfusion-free-release rpmfusion-nonfree-release
+
+dnf -y copr enable atim/heroic-games-launcher
+dnf -y copr enable ilyaz/LACT
+dnf -y install heroic-games-launcher-bin
+dnf -y install lact
+dnf -y copr disable atim/heroic-games-launcher
+dnf -y copr disable ilyaz/LACT
 # Use a COPR Example:
 #
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
+# dnf -y copr enable ublue-os/staging
+# dnf -y install package
 # Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
+# dnf -y copr disable ublue-os/staging
 
 #### Example for enabling a System Unit File
 
